287acac51df6c227fd09cb9c4dcdc2cd8db39401
Naman003 <83799077+Naman003@users.noreply.github.com>
2022-02-24 11:33:17 +0530

    Update client_t.py

--- client_t.py
+++ client_t.py
@@ -2,7 +2,7 @@ import socket
 while True:
     name = input("Please enter filename to be stored in: ")
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect(('172.20.10.9',1000))
+    client.connect(('172.20.10.3',1000))
     file = open(name,'wb')
     chunk = client.recv(1024)
     while chunk:
