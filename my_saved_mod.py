3034fd37c34178923b05b3a804b56d5cc01825ea
Naman003 <83799077+Naman003@users.noreply.github.com>
2022-02-27 09:41:48 +0530

    Update my_saved_mod.py

--- my_saved_mod.py
+++ my_saved_mod.py
@@ -5,6 +5,6 @@ import numpy as np
 from tensorflow.keras.models import load_model as md
 from yolov3.utils import detect_image
 from yolov3.configs import *
-yolo_new = md('mymod.h5',compile=False)
+yolo_new = md('/home/pi/fcsd/yolov3_fcsd/mymod.h5',compile=False)
 count = detect_image(yolo_new, "traffic_2.webp","pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
 print(count)
